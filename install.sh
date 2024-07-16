2978cf1f85a08f30ee0ab8fed397648023922169
daodesigner <designerdao.eth@gmail.com>
2024-07-16 09:56:01 -0400

    update install

--- install.sh
+++ install.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
-REPOOWNER="ekkinox"
+REPOOWNER="daodesigner"
 REPONAME="yai"
 RELEASETAG=$(curl -s "https://api.github.com/repos/$REPOOWNER/$REPONAME/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
 KERNEL=$(uname -s 2>/dev/null || /usr/bin/uname -s)
 case ${KERNEL} in
-    "Linux"|"linux")
-        KERNEL="linux"
-        ;;
-    "Darwin"|"darwin")
-        KERNEL="darwin"
-        ;;
-    *)
-        output "OS '${KERNEL}' not supported" "error"
-        exit 1
-        ;;
+"Linux" | "linux")
+  KERNEL="linux"
+  ;;
+"Darwin" | "darwin")
+  KERNEL="darwin"
+  ;;
+*)
+  output "OS '${KERNEL}' not supported" "error"
+  exit 1
+  ;;
 esac
 
 MACHINE=$(uname -m 2>/dev/null || /usr/bin/uname -m)
 case ${MACHINE} in
-    arm|armv7*)
-        MACHINE="arm"
-        ;;
-    aarch64*|armv8*|arm64)
-        MACHINE="arm64"
-        ;;
-    i[36]86)
-        MACHINE="386"
-        if [ "darwin" = "${KERNEL}" ]; then
-            output "  [ ] Your architecture (${MACHINE}) is not supported anymore" "error"
-            exit 1
-        fi
-        ;;
-    x86_64)
-        MACHINE="amd64"
-        ;;
-    *)
-        output "  [ ] Your architecture (${MACHINE}) is not currently supported" "error"
-        exit 1
-        ;;
+arm | armv7*)
+  MACHINE="arm"
+  ;;
+aarch64* | armv8* | arm64)
+  MACHINE="arm64"
+  ;;
+i[36]86)
+  MACHINE="386"
+  if [ "darwin" = "${KERNEL}" ]; then
+    output "  [ ] Your architecture (${MACHINE}) is not supported anymore" "error"
+    exit 1
+  fi
+  ;;
+x86_64)
+  MACHINE="amd64"
+  ;;
+*)
+  output "  [ ] Your architecture (${MACHINE}) is not currently supported" "error"
+  exit 1
+  ;;
 esac
 
 BINNAME="${BINNAME:-yai}"
@@ -57,3 +57,4 @@ rm "yai_${RELEASETAG}_${KERNEL}_${MACHINE}.tar.gz"
 
 echo
 echo "Installation of version $RELEASETAG complete!"
+
